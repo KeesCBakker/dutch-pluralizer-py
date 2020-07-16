d48e5bca7f3b5e115adb46d6aa85a80ba003c4c5
Kees C. Bakker <k.bakker@hostedsolutions.nu>
2020-07-16 09:32:00 +0200

    Bump version: 0.0.21 → 0.0.22

--- dutch_pluralizer/__init__.py
+++ dutch_pluralizer/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.21"
+__version__ = "0.0.22"
 
 __all__ = ['pluralize', 'pluralize_advanced', 'singularize',
            'could_be_plural', 'singularize_advanced', 'NounMap', 'Pluralizer']
