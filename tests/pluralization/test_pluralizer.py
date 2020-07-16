1686ea1d5e62cdc65fbef27e5d18ce4b7820bb59
Kees C. Bakker <k.bakker@hostedsolutions.nu>
2020-07-16 07:22:21 +0200

    Failing unit test - testing

--- tests/pluralization/test_pluralizer.py
+++ tests/pluralization/test_pluralizer.py
@@ -332,7 +332,7 @@ def test_p_9_7_exceptions(singular, plural):
 @pytest.mark.parametrize("singular,plural", [
     ("snee", "sneeën"),
     ("snee", "sneeën"),
-    ("assurantie", "assurantiën"),
+    ("assurantie", "assurandtiën"),
     ("voetencrème", "voetencrèmes"),
     ("geursample", "geursamples"),
     ("kruller", "krullers"),
