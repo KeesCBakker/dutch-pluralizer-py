9547c45ac582ae4c3473f3d36e622490677b0ac4
Kees C. Bakker <k.bakker@hostedsolutions.nu>
2020-07-19 08:02:20 +0200

    try / except to catch import fails.

--- dutch_pluralizer/__init__.py
+++ dutch_pluralizer/__init__.py
@@ -3,7 +3,12 @@ __version__ = "0.0.33"
 __all__ = ['pluralize', 'pluralize_advanced', 'singularize',
            'could_be_plural', 'singularize_advanced', 'NounEndingMap', 'Pluralizer']
 
-from hunspell import Hunspell
+try:
+    from hunspell import Hunspell
+except:
+    #do nothing
+    a = 1
+
 
 from .mapping import NounEndingMap
 from .pluralizer import pluralize, pluralize_advanced
