3e6eb894f2c53f4f8ed41ba584af46a68fa99985
Kees C. Bakker <k.bakker@hostedsolutions.nu>
2020-12-23 14:53:03 +0100

    Signal support for other versions of Python

--- setup.py
+++ setup.py
@@ -20,7 +20,9 @@ setup(
     license="MIT",
     classifiers=[
         "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8"
     ],
     packages=["dutch_pluralizer"],
     include_package_data=True,
